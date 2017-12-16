b84db63564b51b6384fdd711127a0aa016cbb68f
ScottatVertafore <sdouglas@vertafore.com>
2017-12-16 17:52:19 -0500

    updated assignments

---
+++ WebFundamentals/Javascript/Birthday/script.js
@@ -0,0 +1,10 @@
+        
+        
+        for (var num = 0; num >30; num=num-1)
+        {
+            console.log(num + " days to my birthday. It's as far away as another galaxy.");
+        }
+            console.log("its your birthday");    
+day");    
+y");    
+    </script>
---
+++ WebFundamentals/Javascript/FancyArray/script.js
@@ -0,0 +1,20 @@
+var arr = ["James","Jill","Jane","Jack"];
+var length=0;
+console.log(arr[length]);
+       
+    
+
+
+    
+    
+
+
+  
+
+ 
+ {
+        console.log(length + "=>" + arr[length]);
+        length++;
+    }
+
+}
